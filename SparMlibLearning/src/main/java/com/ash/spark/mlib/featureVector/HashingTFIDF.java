486b79f00f1c2ab97b9cdf40fef993b06336ae06
Amit-Hora <hora.amit@gmail.com>
2016-05-24 13:08:10 +0530

    Added comments explaining the transformer/mode

--- SparMlibLearning/src/main/java/com/ash/spark/mlib/featureVector/HashingTFIDF.java
+++ SparMlibLearning/src/main/java/com/ash/spark/mlib/featureVector/HashingTFIDF.java
@@ -20,6 +20,8 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
 import io.netty.util.internal.JavassistTypeParameterMatcherGenerator;
+import scala.Function1;
+import scala.runtime.BoxedUnit;
 public class HashingTFIDF {
 
 	public static void main(String args[]){
@@ -28,6 +30,7 @@ public class HashingTFIDF {
 		conf.setAppName("MlibLearning").setMaster("local[1]");
 		JavaSparkContext jsc=new JavaSparkContext(conf);
 		JavaRDD<Row> jrdd = jsc.parallelize(Arrays.asList(
+				//RowFctory.create(Object ...values)
 				  RowFactory.create(0, "Hi I heard about Spark"),
 				  RowFactory.create(0, "I wish Java could use case classes"),
 				  RowFactory.create(1, "Logistic regression models are neat")
@@ -42,22 +45,41 @@ public class HashingTFIDF {
 			
 		});
 				StructType schema = new StructType(new StructField[]{
-				  new StructField("label", DataTypes.DoubleType, false, Metadata.empty()),
-				  new StructField("sentence", DataTypes.StringType, false, Metadata.empty())
+									//name 			DataType	can be null 	metadata
+				  new StructField("label", DataTypes.DoubleType, false, Metadata.empty()),//label either 0 or 1
+				  new StructField("sentence", DataTypes.StringType, false, Metadata.empty()) //complete scentence
 				});
 				SQLContext sqlContext=new SQLContext(jsc);
-				DataFrame sentenceData = sqlContext.createDataFrame(jrdd, schema);
-				Tokenizer tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words");
+				DataFrame sentenceData = sqlContext.createDataFrame(jrdd, schema); //applied schema on Row RDD
+				
+				
+				Tokenizer tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words");//splits each scentence into words
+				
+				
 				DataFrame wordsData = tokenizer.transform(sentenceData);
+				wordsData.select("words").show();
+				wordsData.select("sentence").show();
+				
 				int numFeatures = 20;
 				HashingTF hashingTF = new HashingTF()
 				  .setInputCol("words")
 				  .setOutputCol("rawFeatures")
 				  .setNumFeatures(numFeatures);
-				DataFrame featurizedData = hashingTF.transform(wordsData);
+				
+				//hashingTF is a transformer which takes set of terms and converts those set into fixed length feature vectors
+				// here a set of terms is the bag of words //with input col as words ab outpts the feature in rawFeatures
+				
+				DataFrame featurizedData = hashingTF.transform(wordsData); //a transformer transforms a data frame of one type tp other type
+				
+				//featurized Data has the feature vector representing all the sentences  
+				featurizedData.show();
+				
 				IDF idf = new IDF().setInputCol("rawFeatures").setOutputCol("features");
-				IDFModel idfModel = idf.fit(featurizedData);
+				//an IDF is an estimator that fits on a dataset and produces IDF model
+				
+				IDFModel idfModel = idf.fit(featurizedData);//featurizedData is the data set here on which the idf will fit to create the model
 				DataFrame rescaledData = idfModel.transform(featurizedData);
+				// the model than transforms the  rawfeatures to features by scalling them down based on the number of feature supplied
 				for (Row r : rescaledData.select("features", "label").take(3)) {
 				  Vector features = r.getAs(0);
 				  Double label = r.getDouble(1);
@@ -66,3 +88,4 @@ public class HashingTFIDF {
 				}
 	}
 }
+
